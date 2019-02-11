baead0572a1e4bc8be599de20e72a88e34a9c098
kanimozhi <45929166+16CS036@users.noreply.github.com>
2019-02-11 22:03:04 +0530

    Create set1-3.c

---
+++ set1-3.c
@@ -0,0 +1,16 @@
+int main()
+{
+    char a;
+    scanf("%c",&a);
+    if(a=='a'||a=='A'||a=='e'||a=='E'||a=='i'||a=='I'||a=='o'||a=='O'||a=='u'||a=='U')
+    {
+        printf("a is vowel");
+        
+    }
+    else 
+    {
+        printf("a is constant");
+    }
+
+    return 0;
+}
