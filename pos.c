00c117ac706b4c4badd58f17605d2306995c5aa9
kanimozhi <45929166+16CS036@users.noreply.github.com>
2019-02-11 16:23:50 +0530

    Create pos.c

---
+++ pos.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+#include<conio.h>
+
+int main()
+
+{
+
+int a;
+
+printf("enter the integer value:");
+
+scanf("%d",&a);
+
+if(a>0)
+
+{
+
+printf("possitive number:");
+
+}
+
+if(a==0)
+
+{
+
+printf("zero");
+}
+else
+
+{
+
+printf("negative");
+
+}
+
+getch();
+
+
+}
