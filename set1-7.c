de83cc9fb9f348251549d14f7d71ccaa9630707f
kanimozhi <45929166+16CS036@users.noreply.github.com>
2019-02-12 21:11:44 +0530

    Create set1-7.c

---
+++ set1-7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+      char s[20];
+   int i,n;
+   scanf("%s",s);
+   printf("enter the limits");
+   scanf("%d",&n);
+   for(i=0;i<n;i++)
+{
+printf("\n%s",s);
+}
+
+    return 0;
+}
