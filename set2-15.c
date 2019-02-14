85dd0c1bdfa90d6dc198de401ae04f0c47db40fb
kanimozhi <45929166+16CS036@users.noreply.github.com>
2019-02-14 16:18:31 +0530

    Create set2-15.c

---
+++ set2-15.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{ 
+int a,b,i;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    for(i=a+2;i<b;i++)
+    {
+    if(i%2==0)
+    {
+        printf("%d",i);
+    }
+    }
+    return 0;
+}
