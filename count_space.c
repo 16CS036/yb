3bbdedccd8def0e42ee52da3df7a79970cc23a46
kanimozhi <45929166+16CS036@users.noreply.github.com>
2019-02-27 21:26:45 +0530

    Create count_space.c

---
+++ count_space.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<string.h>
+
+int main()
+{
+   char s[100];
+   int i,l,count=0;
+   printf("enter the string");
+   scanf("%[^\n]%*c",s);
+   l=strlen(s);
+   for(i=0;i<l;i++)
+   {
+       if(s[i]==' ')
+       {
+       count++;
+       
+       }
+       
+   }
+   printf("%d",count);
+   
+
+    return 0;
+}
