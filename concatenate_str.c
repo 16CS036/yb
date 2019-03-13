3e7dd9d9699d782ec59141aa205c6526003b6845
kanimozhi <45929166+16CS036@users.noreply.github.com>
2019-03-13 15:56:25 +0530

    Create concatenate_str.c

---
+++ concatenate_str.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    char s[10],g[10];
+    int l,k;
+    printf("enter the first string");
+    scanf("%s",s);
+    printf("enter the second string");
+    scanf("%s",g);
+    printf("%s%s",s,g);
+
+    return 0;
+}
