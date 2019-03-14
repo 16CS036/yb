f0b0a48f53e019b0ea8c0e19a7e3a2bce3c9a9cf
kanimozhi <45929166+16CS036@users.noreply.github.com>
2019-03-14 16:22:38 +0530

    Create nearesteven_num.c

---
+++ nearesteven_num.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,r,sum=0;
+  scanf ("%d", &n);
+ 
+      if(n%2==0)
+      {
+          printf("%d",n);
+      }
+      else
+      {
+          n--;
+          printf("%d",n);
+      }
+  
+
+    return 0;
+}
