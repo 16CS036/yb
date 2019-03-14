ad46fdacd0a237a5ba415ea581a7638f73d48aea
kanimozhi <45929166+16CS036@users.noreply.github.com>
2019-03-14 16:31:32 +0530

    Create product_num.c

---
+++ product_num.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int n,r,pro;
+  scanf ("%d", &n);
+  scanf("%d",&r);
+  pro=n*r;
+  if(pro%2==0)
+  {
+      printf("even");
+  }
+  else
+  {
+      printf("no");
+  }
+  
+
+    return 0;
+}
