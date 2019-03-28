fc081863af717142a764f4618adeda49e8a804e4
kanimozhi <45929166+16CS036@users.noreply.github.com>
2019-03-28 16:19:22 +0530

    Create prime_num.c

---
+++ prime_num.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,i,flag=0;
+	printf("enter the value");
+	scanf("%d",&n);
+	for(i=2;i<=n/2;i++)
+	{
+	    if(n%i==0)
+	    {
+	        flag=1;
+	    }
+	}
+	if(flag==0)
+	{
+	    printf("prime");
+	}
+	else
+	{
+	    printf("Not prime");
+	}
+
+    return 0;
+}
