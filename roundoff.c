dcb197fd0e9a7538092e91377c8f62b23f1d684d
kanimozhi <45929166+16CS036@users.noreply.github.com>
2019-04-01 16:32:30 +0530

    Create roundoff.c

---
+++ roundoff.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    float n;
+    int a;
+    printf("enter the number");
+    scanf("%f",&n);
+    a=(int)(n+0.5);
+    
+    printf("%d",a);
+
+    return 0;
+}
