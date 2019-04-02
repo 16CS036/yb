f2e5c8f927b37794d7a717b6a691778d94633a9a
kanimozhi <45929166+16CS036@users.noreply.github.com>
2019-04-02 19:58:29 +0530

    Create multipleof13.c

---
+++ multipleof13.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("enter the integer");
+    scanf("%d",&n);
+    if(n%13==0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+
+    return 0;
+}
