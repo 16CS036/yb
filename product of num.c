5ef3998210fdddaf8feb92dd918f4ab31bd193df
kanimozhi <45929166+16CS036@users.noreply.github.com>
2019-06-18 21:50:15 +0530

    Create product of num.c

---
+++ product of num.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,s=1,r;
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        r=n%10;
+        s=r*s;
+        n=n/10;
+    }
+    printf("%d",s);
+
+    return 0;
+}
